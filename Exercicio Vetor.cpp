f2c7baa231141c4a559f1e799f3e82e7ad87893f
wendler <wendlerconstancio@gmail.com>
2023-08-31 23:51:20 -0300

    Exercício Vetor

---
+++ Exercicio Vetor.cpp
@@ -0,0 +1,46 @@
+#include <iostream> 
+#include <locale.h>
+
+
+using namespace std;
+
+int main(){
+	
+
+	setlocale(LC_ALL, "Portuguese");
+	
+	int contador, posicao;
+	int vetorPrincipal[5], vetorNumerosImpares[5];
+	
+	
+	for( posicao = 0; posicao < 5; posicao++ ){
+		
+
+		cout << "Digite o número da posição " << posicao << ": ";
+		cin >> vetorPrincipal[posicao];
+		
+
+		if( vetorPrincipal[posicao] % 2 != 0 ){
+			
+			vetorNumerosImpares[contador] = vetorPrincipal[posicao];
+			contador++;
+			
+		}
+		
+	}
+	
+	cout << "\n\nOs números impares são: \n";
+	cout << "Total impares: " << contador << "\n";
+	
+
+	for( posicao = 0; posicao < contador; posicao++){
+		
+		cout << vetorNumerosImpares[posicao] << "\n";
+		
+	}
+	
+			
+	
+	system("pause");
+	return 0;
+}
